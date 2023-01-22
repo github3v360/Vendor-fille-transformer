8bb0cd0ff64029a4a4d46a249b02f73e122d5702
github3v360 <github3.v360@gmail.com>
2023-01-22 04:39:12 +0000

    hyperlink testing updated

--- src/hyperlink-extraction-testing.py
+++ src/hyperlink-extraction-testing.py
@@ -1,3 +1,4 @@
+from openpyxl import load_workbook
 import openpyxl
 import os
 import pandas as pd
@@ -6,10 +7,13 @@ from src.utils import data_cleaner
 test_data_dir  = os.path.join("artifacts","test_data")
 test_data_files_name =  os.listdir(test_data_dir)
 
+import pandas as pd
+from openpyxl import load_workbook
+
 for file_name in test_data_files_name[:]:
     print()
     print(f" =========== File Name : {file_name} ============")
-    df = pd.read_excel(os.path.join(test_data_dir,file_name), None);
+    df = pd.read_excel(os.path.join(test_data_dir,file_name), None)
     sheet_names = list(df.keys())
     cur_sheet = df[sheet_names[0]]
     columns_name = list(cur_sheet.columns)
@@ -23,10 +27,14 @@ for file_name in test_data_files_name[:]:
         
         cur_link = ws.cell(row = 20, column = cur_col).hyperlink
         if cur_link is not None:
-            if cur_link.display is not None:
+            if cur_link.target is not None:
                 cols_link.append((columns_name[cur_col-1],cur_col))
     
     # print(ws.max_row)
+    print(cols_link)
+    # for j in range(len(cols_link)):
+    #     print(columns_name[cols_link[j][1]-1])
+    #     print(ws.cell(20,cols_link[j][1]).hyperlink.target)
     
 
 
@@ -47,6 +55,9 @@ for file_name in test_data_files_name[:]:
     for i in range(correct_row_idx+3,ws.max_row+1):
 
         for j in range(len(cols_link)):
-            df_link[df.columns[cols_link[j][1]-1] + "_link"].iloc[t] = ws.cell(row=i,column=cols_link[j][1]).hyperlink.display
+            if ws.cell(row=i,column=cols_link[j][1]).hyperlink is None:
+                df_link[df.columns[cols_link[j][1]-1] + "_link"].iloc[t] = None
+                continue
+            df_link[df.columns[cols_link[j][1]-1] + "_link"].iloc[t] = ws.cell(row=i,column=cols_link[j][1]).hyperlink.target
         t+=1
     print(df_link.head(2))
