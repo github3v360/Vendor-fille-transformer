0d702d58b8ae928793ab2a704c98c100637561ac
github3v360 <github3.v360@gmail.com>
2023-01-22 06:49:37 +0000

    main.py updated

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ import fnmatch
 import pickle
 import yaml
 import pandas as pd
-from src.utils.all_utils import *
+from src import Extraction_of_entire_file
 
 # Bucket Realted parameters and functions
 
@@ -79,148 +79,6 @@ def addSummaryFileMeta(summaryFilePath, uid, VENDORNAME):
 
   collection_ref.add(data)
 
-def onInventoryFileUpload(inventory, uid, VENDORNAME, CREATEDAT):
-    inventory['VENDORNAME'] = VENDORNAME
-
-    start_time = time.time()
-    try:
-        
-        masterFilePath = get_master_file_path(uid);
-        downloadFromBucket(
-            master_bucket,
-            masterFilePath,
-            os.path.join(tempdir, 'master.xlsx')
-        )
-        
-    except Exception:
-        
-        masterColumns = {}
-        print('exceptiong new dataframe columns',inventory.columns)
-
-        for col in inventory.columns:
-            masterColumns[col] = []
-            
-
-        for col in ['CREATEDAT', 'LASTMODIFIEDAT']:
-            masterColumns[col] = []
-
-        emptyMasterFile = pd.DataFrame(columns = masterColumns)
-        print("new dataframe created")
-        exportDataFrameToExcel(emptyMasterFile, os.path.join(tempdir, 'master.xlsx'))
-    print(3)
-    
-    master = read_excel(os.path.join(tempdir,'master.xlsx'))
-    wb_master = openpyxl.load_workbook(os.path.join(tempdir,'master.xlsx'),data_only = True)
-    print(3.1)
-    master = mapping(master,wb_master.worksheets[0])
-    print(3.2)
-    print("master columns",master.columns)
-    # inventory['REPORTNO'].replace('','0',inplace=True)
-    # fileValue_df.fillna(value=0,inplace=True)
-    # master['REPORTNO'] = master['REPORTNO'].astype(np.float64).astype(np.int64)
-    # inventory['REPORTNO'] = inventory['REPORTNO'].astype(np.float64).astype(np.int64)
-    print(4)
-    masterDate= master.merge(inventory,how='outer',on='REPORTNO',indicator=True)
-    print(masterDate.columns)
-    print(5)
-    masterDate['CREATEDAT'] = masterDate['CREATEDAT'].astype(str)
-    masterDate['LASTMODIFIEDAT'] = masterDate['LASTMODIFIEDAT'].astype(str)
-    masterDate['CREATEDAT'] = masterDate.apply(lambda row : mapDate(row, CREATEDAT), axis = 1)
-    print(masterDate)
-    print(5.1)
-    masterDate['LASTMODIFIEDAT'] = masterDate.apply(lambda row : mapDate2(row, CREATEDAT), axis = 1)
-    print(5.2)
-
-    summary = masterDate[masterDate['_merge'] != 'left_only' ]
-    print("5.3",summary)
-    summary['summary'] = summary.apply(lambda row : map(row), axis = 1)
-    print(5.4)
-    summary['%Change']= summary.apply(lambda row: mapDiff(row),axis=1)
-    print(5.5)
-    summary = summary[[column for column in summary.columns if '_y' in column] + ['REPORTNO','CREATEDAT','%Change','summary','LASTMODIFIEDAT']]
-    summary=summary.rename(columns={column:column.replace("_y", "") for column in summary.columns})
-    print(5.6)
-    summary['REPORTNOabc']=summary['REPORTNO']
-    summary['%Changeabc']=summary['%Change']
-    summary['summaryabc']=summary['summary']
-
-    summary=summary.drop(['REPORTNO','%Change','summary'], axis = 1)
-    summary=summary[[column for column in summary.columns if 'abc' in column]+ [column for column in summary.columns if 'abc' not in column]]
-    summary=summary.rename(columns={column:column.replace("abc", "") for column in summary.columns})
-
-    print(summary.columns)
-    
-    masterDate=masterDate[[column for column in masterDate.columns if '_y' not in column]] 
-    masterDate=masterDate.rename(columns={column:column.replace("_x", "") for column in masterDate.columns})
-    print(masterDate.columns)
-
-    masterDate=pd.concat([masterDate,summary]).drop_duplicates(subset=['REPORTNO'],keep='last')
-    masterDate=masterDate.drop(['summary','_merge','%Change'],axis=1)
-
-    
-    print(6)
-    masterDate['REPORTNOabc']=masterDate['REPORTNO']
-
-    # masterDate['VIDEOLINKabc']=masterDate['VIDEOLINK']
-    # masterDate=masterDate.drop(['REPORTNO','VIDEOLINK'], axis = 1)
-    masterDate=masterDate.drop(['REPORTNO'], axis = 1)
-    masterDate=masterDate[[column for column in masterDate.columns if 'abc' in column]+ [column for column in masterDate.columns if 'abc' not in column]]
-    masterDate=masterDate.rename(columns={column:column.replace("abc", "") for column in masterDate.columns})
-    masterDate = masterDate.loc[:, masterDate.columns.notna()]
-    print("masterDate columns", masterDate.columns)
-    
-    print(8)
-    start_time = time.time()
-    exportDataFrameToExcel(summary, os.path.join(tempdir, 'summary.xlsx'))
-    summaryFilePath = '/'.join([uid, str(uuid.uuid4()), 'summary.xlsx'])
-
-    uploadToBucket(
-        summary_bucket,
-        summaryFilePath,
-        os.path.join(tempdir, 'summary.xlsx')
-    )
-    
-    addSummaryFileMeta(summaryFilePath, uid, VENDORNAME)
-    print(9)
-    start_time = time.time()
-
-    exportDataFrameToExcel(masterDate, os.path.join(tempdir, 'master.xlsx'))
-
-    folder =  str(uuid.uuid4())
-    newMasterFilePath = '/'.join([uid, folder, 'master.xlsx'])
-
-    uploadToBucket(
-        master_bucket,
-        newMasterFilePath,
-        os.path.join(tempdir, 'master.xlsx')
-    )
-
-    with open(os.path.join(tempdir, 'master.pickle'), 'wb+') as pickleFile:
-        pickle.dump(masterDate, pickleFile)
-
-    uploadToBucket(
-        master_bucket,
-        '/'.join([uid, folder, 'master.pickle']),
-        os.path.join(tempdir, 'master.pickle')
-    )
-    
-    addMasterFileMeta(newMasterFilePath, uid, VENDORNAME)
-    print(10)
-    print("{} entire data analysis code took ".format(time.time() - start_time))
-
-def uploadToBucket(bucketName, path, filepath):
-        bucket = client.get_bucket(bucketName)
-
-        blob = bucket.blob(path)
-        
-        with open(filepath, 'rb') as file:
-            blob.upload_from_file(file)
-
-        blob.make_public()
-
-def exportDataFrameToExcel(dataframe, path):
-        dataframe.to_excel(path)
-
 def helloFirestore(event, context):
     """
     Triggered by a change to a Firestore document.
@@ -228,8 +86,7 @@ def helloFirestore(event, context):
         event (dict): Event payload.
         context (google.cloud.functions.Context): Metadata for the event.
     """
-    print(event)
-    resource_string = context.resource
+    
     bucketName= event['value']['fields']['bucket']['stringValue']
     bucket = client.get_bucket(bucketName)
 
@@ -240,37 +97,16 @@ def helloFirestore(event, context):
     
     for everyobj in bucketPathArray:
         currentFilePath=everyobj['mapValue']['fields']['filePath']['stringValue']
-        print(currentFilePath)
+    
         blob=bucket.blob(currentFilePath)
         blob.download_to_filename(os.path.join(tempdir, currentFilePath.split('/')[-1]))
-        fileValue=pd.ExcelFile(os.path.join(tempdir, currentFilePath.split('/')[-1]))
-        list_of_sheetname=fileValue.sheet_names
-        print("======Listing sheet name =======")
-        print(list_of_sheetname)
-        global_df=pd.DataFrame()
-        global_df['REPORTNO']=[]
-        wb = openpyxl.load_workbook(os.path.join(tempdir, currentFilePath.split('/')[-1]),data_only = True)
-        for i in list_of_sheetname:
-            fd=pd.read_excel(os.path.join(tempdir, currentFilePath.split('/')[-1]),sheet_name=i,header=None)
-            print("=========fd with header None =======")
-            print(fd.columns)
-            print(fd.head())
-            fd=pd.read_excel(os.path.join(tempdir, currentFilePath.split('/')[-1]),sheet_name=i)
-            print("=========fd with header not None =======")
-            print(fd.columns)
-            print(fd.head())
-            global_df=pd.concat([global_df,fd], axis=0)
-        global_df=global_df.reset_index()
-        global_df = global_df.drop(['index'], axis=1, errors='ignore')
+
+        out_df = Extraction_of_entire_file.extract_entire_file(os.path.join(tempdir, currentFilePath.split('/')[-1]),debug=False)
+        out_df=out_df.reset_index()
+
         userId=currentFilePath.split('/')[0]
         filenames.append(os.path.join(tempdir,currentFilePath.split('/')[-1]))
-        print("===========Printing global_df =============")
-        print(global_df.head())
-        out_df = transform_df(global_df)
-        print("===========Printing out df=============")
-
-        print(out_df.head())
-
+    
         summary_bucket = os.environ['SUMMARY_BUCKET']
         exportDataFrameToExcel(out_df, os.path.join(tempdir, 'summary_2.xlsx'))
         summaryFilePath = '/'.join([userId, str(uuid.uuid4()), 'summary_2.xlsx'])
