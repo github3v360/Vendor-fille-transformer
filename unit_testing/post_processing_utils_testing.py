565fb26c62ec2375a4d08bceb6ddc60f3a9577fe
Tirth1306 <80909124+github3v360@users.noreply.github.com>
2023-01-28 05:59:03 -0500

    Kanisha added Unit Testing for Cut Column

--- unit_testing/post_processing_utils_testing.py
+++ unit_testing/post_processing_utils_testing.py
@@ -41,6 +41,35 @@ class TestTransformShapeColumn(unittest.TestCase):
     result = post_processing_utils.transform_shape_column(None,magic_numbers)
     self.assertIsNone(result)
 
+class TestTransformCutColumn(unittest.TestCase):
+    def test_transform_cut_column(self):
+    
+        magic_numbers = common_utils.read_yaml("params.yaml")['magic_numbers']
+
+        def test_transform_cut_column_none(self):
+            cut_val = ""
+            magic_numbers = {"cut_similarity_transform_df_threshold": 0.8}
+            result = post_processing_utils.transform_cut_column(cut_val, magic_numbers)
+            self.assertEqual(result, None)
+
+        def test_transform_cut_column_exact_match(self):
+            cut_val = "I"
+            magic_numbers = {"cut_similarity_transform_df_threshold": 0.8}
+            result = post_processing_utils.transform_cut_column(cut_val, magic_numbers)
+            self.assertEqual(result, "I")
+
+        def test_transform_cut_column_similar_match(self):
+            cut_val = "in"
+            magic_numbers = {"cut_similarity_transform_df_threshold": 0.8}
+            result = post_processing_utils.transform_cut_column(cut_val, magic_numbers)
+            self.assertEqual(result, "I")
+
+        def test_transform_cut_column_no_match(self):
+            cut_val = "abc"
+            magic_numbers = {"cut_similarity_transform_df_threshold": 0.8}
+            result = post_processing_utils.transform_cut_column(cut_val, magic_numbers)
+            self.assertEqual(result, None)    
+
 
 
 if __name__ == "__main__":
