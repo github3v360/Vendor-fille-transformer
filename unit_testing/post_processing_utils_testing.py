730f65a6c5dd0d559caf76610ad898ebd7440afc
Tirth1306 <80909124+github3v360@users.noreply.github.com>
2023-01-29 02:28:25 -0500

    post_processing test utils updated

--- unit_testing/post_processing_utils_testing.py
+++ unit_testing/post_processing_utils_testing.py
@@ -41,6 +41,62 @@ class TestTransformShapeColumn(unittest.TestCase):
     result = post_processing_utils.transform_shape_column(None,magic_numbers)
     self.assertIsNone(result)
 
+class TestTransformfluorColumn(unittest.TestCase):
+  def test_transform_fluor_column(self):
+
+    magic_numbers = common_utils.read_yaml("params.yaml")['magic_numbers']
+
+    # Test with a fluorescent that exists in the fluor dictionary
+    result = post_processing_utils.transform_fluor_column("faint",magic_numbers)
+    self.assertEqual(result, "FAINT")
+
+    # Test with a fluorescent that exists in the fluor dictionary
+    result = post_processing_utils.transform_fluor_column("f",magic_numbers)
+    self.assertEqual(result, "FAINT")
+
+    # Test with a fluorescent that exists in the fluor dictionary
+    result = post_processing_utils.transform_fluor_column("non",magic_numbers)
+    self.assertEqual(result, "NONE")
+
+    # Test with a fluorescent that don't exists in the fluor dictionary
+    result = post_processing_utils.transform_fluor_column("med-w",magic_numbers)
+    self.assertEqual(result, "MEDIUM")
+
+    # Test with a fluorescent that exists in the fluor dictionary
+    result = post_processing_utils.transform_fluor_column("",magic_numbers)
+    self.assertEqual(result, None)
+
+    result = post_processing_utils.transform_fluor_column(None,magic_numbers)
+    self.assertEqual(result, None)
+
+
+class TestTransformMeasurementColumn(unittest.TestCase):
+  def test_transform_measurement_column(self):
+
+      result = post_processing_utils.transform_measurement_column("2*3*4")
+      self.assertEqual(result,[4,3,2])
+
+      result = post_processing_utils.transform_measurement_column(" 2  *     3*    4 ")
+      self.assertEqual(result,[4,3,2])
+
+      result = post_processing_utils.transform_measurement_column("2+3+4")
+      self.assertEqual(result,[4,3,2])
+
+      result = post_processing_utils.transform_measurement_column("2*3+4")
+      self.assertEqual(result,[4,3,2])
+
+      result = post_processing_utils.transform_measurement_column("2.2*3.2*4.2")
+      self.assertEqual(result,[4.2,3.2,2.2])
+
+      result = post_processing_utils.transform_measurement_column(None)
+      self.assertEqual(result,[None,None,None])
+
+      result = post_processing_utils.transform_measurement_column("")
+      self.assertEqual(result,[None,None,None])
+
+      result = post_processing_utils.transform_measurement_column(1)
+      self.assertEqual(result,[None,None,None])
+      
 class TestTransformCutColumn(unittest.TestCase):
     def test_transform_cut_column(self):
     
