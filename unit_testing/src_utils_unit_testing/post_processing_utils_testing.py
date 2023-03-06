180caeb4fe5a346653d82deb0bd53fc16da764f1
Tirth1306 <80909124+github3v360@users.noreply.github.com>
2023-03-06 06:45:51 -0700

    Kanisha changed unit testing for post_processing_unit testing

--- unit_testing/src_utils_unit_testing/post_processing_utils_testing.py
+++ unit_testing/src_utils_unit_testing/post_processing_utils_testing.py
@@ -10,35 +10,31 @@ class TestTransformShapeColumn(unittest.TestCase):
     magic_numbers = common_utils.read_yaml("params.yaml")['magic_numbers']
 
     # Test with a shape that exists in the shape dictionary
-    result = post_processing_utils.transform_shape_column("RND",magic_numbers)
+    result = post_processing_utils.transform_column("RND",magic_numbers,'shape')
     self.assertEqual(result, "Round")
     
     # Test with a shape that is close to a shape in the shape dictionary df['disc'] = df['disc'].astype(float)
-    result = post_processing_utils.transform_shape_column("ROUD",magic_numbers)
+    result = post_processing_utils.transform_column("ROUD",magic_numbers,'shape')
     self.assertEqual(result, "Round")
     
-    # Test with a shape that is not similar to any shape in the shape dictionary
-    result = post_processing_utils.transform_shape_column("orange",magic_numbers)
-    self.assertIsNone(result)
-    
     # Test with a shape that is an exact match to a shape in the shape dictionary
-    result = post_processing_utils.transform_shape_column("OVAL",magic_numbers)
+    result = post_processing_utils.transform_column("OVAL",magic_numbers,'shape')
     self.assertEqual(result, "Oval")
     
     # Test with a shape that is not in the shape dictionary but has a high similarity score
-    result = post_processing_utils.transform_shape_column("OVEL",magic_numbers)
+    result = post_processing_utils.transform_column("OVEL",magic_numbers,'shape')
     self.assertEqual(result, "Oval")
     
     # Test with a shape that is not in the shape dictionary and has a low similarity score
-    result = post_processing_utils.transform_shape_column("V",magic_numbers)
+    result = post_processing_utils.transform_column("V",magic_numbers,'shape')
     self.assertIsNone(result)
     
     # Test with an empty string
-    result = post_processing_utils.transform_shape_column("",magic_numbers)
+    result = post_processing_utils.transform_column("",magic_numbers,'shape')
     self.assertIsNone(result)
     
     # Test with None
-    result = post_processing_utils.transform_shape_column(None,magic_numbers)
+    result = post_processing_utils.transform_column(None,magic_numbers,'shape')
     self.assertIsNone(result)
 
 class TestTransformfluorColumn(unittest.TestCase):
@@ -47,26 +43,26 @@ class TestTransformfluorColumn(unittest.TestCase):
     magic_numbers = common_utils.read_yaml("params.yaml")['magic_numbers']
 
     # Test with a fluorescent that exists in the fluor dictionary
-    result = post_processing_utils.transform_fluor_column("faint",magic_numbers)
+    result = post_processing_utils.transform_column("faint",magic_numbers,"fluor")
     self.assertEqual(result, "FAINT")
 
     # Test with a fluorescent that exists in the fluor dictionary
-    result = post_processing_utils.transform_fluor_column("f",magic_numbers)
+    result = post_processing_utils.transform_column("f",magic_numbers,"fluor")
     self.assertEqual(result, "FAINT")
 
     # Test with a fluorescent that exists in the fluor dictionary
-    result = post_processing_utils.transform_fluor_column("non",magic_numbers)
+    result = post_processing_utils.transform_column("non",magic_numbers,"fluor")
     self.assertEqual(result, "NONE")
 
     # Test with a fluorescent that don't exists in the fluor dictionary
-    result = post_processing_utils.transform_fluor_column("med-w",magic_numbers)
+    result = post_processing_utils.transform_column("med-w",magic_numbers,"fluor")
     self.assertEqual(result, "MEDIUM")
 
     # Test with a fluorescent that exists in the fluor dictionary
-    result = post_processing_utils.transform_fluor_column("",magic_numbers)
+    result = post_processing_utils.transform_column("",magic_numbers,"fluor")
     self.assertEqual(result, None)
 
-    result = post_processing_utils.transform_fluor_column(None,magic_numbers)
+    result = post_processing_utils.transform_column(None,magic_numbers,"fluor")
     self.assertEqual(result, None)
     
 class TestTransformMeasurementColumn(unittest.TestCase):
@@ -167,27 +163,27 @@ class TestTransformCutColumn(unittest.TestCase):
 
     def test_transform_cut_column(self):
         # Test standard shapes
-        assert post_processing_utils.transform_cut_column("I", {"cut_similarity_transform_df_threshold": 0.5}) == "I"
-        assert post_processing_utils.transform_cut_column("EX", {"cut_similarity_transform_df_threshold": 0.5}) == "EX"
-        assert post_processing_utils.transform_cut_column("VG", {"cut_similarity_transform_df_threshold": 0.5}) == "VG"
-        assert post_processing_utils.transform_cut_column("G", {"cut_similarity_transform_df_threshold": 0.5}) == "G"
-        assert post_processing_utils.transform_cut_column("F", {"cut_similarity_transform_df_threshold": 0.5}) == "F"
-        assert post_processing_utils.transform_cut_column("P", {"cut_similarity_transform_df_threshold": 0.5}) == "P"
-        assert post_processing_utils.transform_cut_column("EX+", {"cut_similarity_transform_df_threshold": 0.5}) == "EX"
+        assert post_processing_utils.transform_column("I", {"cut_similarity_transform_df_threshold": 0.5},'cut') == "I"
+        assert post_processing_utils.transform_column("EX", {"cut_similarity_transform_df_threshold": 0.5},'cut') == "EX"
+        assert post_processing_utils.transform_column("VG", {"cut_similarity_transform_df_threshold": 0.5},'cut') == "VG"
+        assert post_processing_utils.transform_column("G", {"cut_similarity_transform_df_threshold": 0.5},'cut') == "G"
+        assert post_processing_utils.transform_column("F", {"cut_similarity_transform_df_threshold": 0.5},'cut') == "F"
+        assert post_processing_utils.transform_column("P", {"cut_similarity_transform_df_threshold": 0.5},'cut') == "P"
+        assert post_processing_utils.transform_column("EX+", {"cut_similarity_transform_df_threshold": 0.5},'cut') == "EX"
 
         # Test non-standard shapes with exact match
-        assert post_processing_utils.transform_cut_column("Round", {"cut_similarity_transform_df_threshold": 0.5}) == None
-        assert post_processing_utils.transform_cut_column("Princess", {"cut_similarity_transform_df_threshold": 0.5}) == None
-        assert post_processing_utils.transform_cut_column("Emerald", {"cut_similarity_transform_df_threshold": 0.5}) == None
+        assert post_processing_utils.transform_column("Round", {"cut_similarity_transform_df_threshold": 0.5},'cut') == None
+        assert post_processing_utils.transform_column("Princess", {"cut_similarity_transform_df_threshold": 0.5},'cut') == None
+        assert post_processing_utils.transform_column("Emerald", {"cut_similarity_transform_df_threshold": 0.5},'cut') == None
 
         # Test non-standard shapes with fuzzy matching
-        assert post_processing_utils.transform_cut_column("RND", {"cut_similarity_transform_df_threshold": 0.5}) == None
-        assert post_processing_utils.transform_cut_column("PRNC", {"cut_similarity_transform_df_threshold": 0.5}) == None
-        assert post_processing_utils.transform_cut_column("EMRLD", {"cut_similarity_transform_df_threshold": 0.5}) == None
+        assert post_processing_utils.transform_column("RND", {"cut_similarity_transform_df_threshold": 0.5},'cut') == None
+        assert post_processing_utils.transform_column("PRNC", {"cut_similarity_transform_df_threshold": 0.5},'cut') == None
+        assert post_processing_utils.transform_column("EMRLD", {"cut_similarity_transform_df_threshold": 0.5},'cut') == None
         
         # Test empty input
-        assert post_processing_utils.transform_cut_column("", {"cut_similarity_transform_df_threshold": 0.5}) == None
-        assert post_processing_utils.transform_cut_column(None, {"cut_similarity_transform_df_threshold": 0.5}) == None
+        assert post_processing_utils.transform_column("", {"cut_similarity_transform_df_threshold": 0.5},'cut') == None
+        assert post_processing_utils.transform_column(None, {"cut_similarity_transform_df_threshold": 0.5},'cut') == None
 
 class TestTransformDiscountColumn(unittest.TestCase):
     def test_transform_discount_column(self):
