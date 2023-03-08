5f2eeaf643e8a46da10d182d591bdc4a65a2ba58
Tirth1306 <80909124+github3v360@users.noreply.github.com>
2023-03-08 02:08:57 -0700

    copied manav stage 1

--- src/Stage_1_data_cleaning_and_link_extraction.py
+++ src/Stage_1_data_cleaning_and_link_extraction.py
@@ -1,27 +1,58 @@
-from src import utils 
+'''
+This file contains the class 'CleanDataAndExtractLink' 
+which will perform all the stage 1 tasks
+'''
+
+from src.utils import data_cleaner
 from src import hyperlink_extraction
-from utils import data_cleaner
 
 class CleanDataAndExtractLink:
-    def __init__(self, df, ws, logger):
-        self.df = df
-        self.ws = ws
+    '''
+    This Class will do the following:
+    1. Correct the headers
+    2. Extract the link
+    3. Extract the report number from the extracted link
+    4. Clean the data (removing empty rows and columns).
+    '''
+    def __init__(self, dataframe, openpyxl_workbook, logger):
+        '''
+        Initializes the instance based on the attributes.
+
+        Attributes:
+            dataframe (pandas dataframe): Dataframe returned
+                      by pandas after reading one of a file's sheets.
+            
+            openpyxl_workbook: openpyxl workbook of one of a file's sheets.
+                               This is used to get the hyperlink since
+                               pandas dataframe do not store the hypelink
+            
+            logger: Python's logger to log the Info, Errors and Exceptions
+        '''
+        self.dataframe = dataframe
+        self.openpyxl_workbook = openpyxl_workbook
         self.logger = logger
 
     def process(self):
 
         """
-        This function will run all the required functions to correct the
-        headers, extract the link, extract the report number from the extracted link
-        and clean the data(removing empty rows and columns).
+        This function will execute all of the necessary utils functions in order to correct the 
+        headers, extract the link, extract the report number from the extracted link, and
+        clean the data (removing empty rows and columns).
+
+        Returns:
+            df_cleanded (pandas DataFrame): A DataFrame with the headers corrected. The
+                                             extracted hyperlinks are also included in 
+                                             this DataFrame.
+            
+            link_columns_name (List): A list of the names of the extracted links
         """
 
         # Correcting the DataFrame Headers
-        df_corrected_headers, correct_row_idx = data_cleaner.correct_df_headers(self.df)
+        df_corrected_headers, correct_row_idx = data_cleaner.correct_df_headers(self.dataframe)
 
         # Extracting the hyperlink and report number from the link
         hyperlink_extractor = hyperlink_extraction.HyperlinkExtractor(
-            self.ws, correct_row_idx, df_corrected_headers
+            self.openpyxl_workbook, correct_row_idx, df_corrected_headers
         )
 
         df_with_links, link_columns_name = hyperlink_extractor.add_hyperlink_columns()
